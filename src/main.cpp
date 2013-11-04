8185551177ec7dc32ae2e911183e03b6f8e36ca0
Barbatos <barbatos@urbanterror.info>
2013-11-04 00:12:49 -0500

    Clean up. Got rid of all the compilation warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,5 @@
 #include "global.hpp"
 
-#ifndef SERVEUR
-	//#include "client/fenetre.hpp"
-#endif
-
-void Frame()
-{
-
-#ifdef SERVEUR
-	SV_Frame();
-#else
-
-	//@Charles - temporaire
-	char temp[1337];
-	int choix;
-
-	cout << "Veuillez ecrire le paquet a envoyer au serveur : " << endl;
-	cout << "	1 : Paquet de connexion au serveur" << endl;
-	cout << "	2 : Paquet de deconnexion au serveur" << endl;
-
-	cin >> temp;
-	choix = atoi(temp);
-
-#endif
-
-	usleep(900000);
-}
-
 int main()
 {
 	return 0;
