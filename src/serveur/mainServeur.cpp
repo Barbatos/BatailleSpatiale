3b7ce3dfd2f649084308e6b03801f4352478e8e5
Barbatos <barbatos@urbanterror.info>
2013-11-09 17:20:52 -0500

    Code pour lancer un serveur

--- src/serveur/mainServeur.cpp
+++ src/serveur/mainServeur.cpp
@@ -1,6 +1,28 @@
 #include "../global.hpp"
+#include "../core/NetworkGlobal.hpp"
+#include "../core/NetworkServer.hpp"
 
 int main()
 {
+	NetworkServer* serveur;
+	unsigned int port = 1337;
+	string msg = "";
+
+	cout << "Vous êtes sur le point de lancer un serveur pour le jeu BattleStar." << endl;
+	cout << "Veuillez entrer le port sur lequel lancer le serveur [" << port << "]" << endl;
+
+	getline(cin, msg);
+	
+	if(!msg.empty()){
+		port = atoi(msg.c_str());
+	}
+
+	serveur = new NetworkServer(port);
+
+	while(true){
+		serveur->acceptNewClient();
+		sf::sleep(sf::milliseconds(50));
+	}
+
 	return 0;
 }
