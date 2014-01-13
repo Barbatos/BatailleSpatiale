e3b241a625373cdeb89ca8c91ddfb8765571b3bb
cmascart <mascart.c@laposte.net>
2014-01-13 12:56:42 +0100

    Ajout de l'empaquetage des positions

--- src/commun/utile/Position.cpp
+++ src/commun/utile/Position.cpp
@@ -1,61 +1,48 @@
 #include "Position.hpp"
-
 Position::Position(int _x, int _y) :
-		x(_x),
-		y(_y)
-{
-
+    x(_x),
+    y(_y) {
 }
-
-bool Position::operator==(Position p) const
-{
-	return x == p.x && y == p.y;
+bool Position::operator==(Position p) const {
+    return x == p.x && y == p.y;
 }
-
-bool Position::isNull() const
-{
-	return x == -32768 && y == -32768;
+bool Position::isNull() const {
+    return x == -32768 && y == -32768;
 }
-
-void Position::operator+=(Position& p)
-{
-	x += p.x;
-	y += p.y;
+void Position::operator+=(Position& p) {
+    x += p.x;
+    y += p.y;
 }
-
-void Position::operator-=(Position& p)
-{
-	x -= p.x;
-	y -= p.y;
+void Position::operator-=(Position& p) {
+    x -= p.x;
+    y -= p.y;
 }
-
-Position operator+(Position& p1, Position& p2)
-{
-	Position p(p1.x, p1.y);
-	p += p2;
-	return p;
+Position operator+(Position& p1, Position& p2) {
+    Position p(p1.x, p1.y);
+    p += p2;
+    return p;
 }
-
-Position operator-(Position& p1, Position& p2)
-{
-	Position p(p1.x, p1.y);
-	p -= p2;
-	return p;
+Position operator-(Position& p1, Position& p2) {
+    Position p(p1.x, p1.y);
+    p -= p2;
+    return p;
 }
-
-void Position::operator=(Position p)
-{
-	set(p.x, p.y);
+void Position::operator=(Position p) {
+    set(p.x, p.y);
 }
-
-void Position::set(int _x, int _y)
-{
-	x = _x;
-	y = _y;
+void Position::set(int _x, int _y) {
+    x = _x;
+    y = _y;
 }
-
-void Position::setNull()
-{
-	x = -32768;
-	y = -32768;
+void Position::setNull() {
+    x = -32768;
+    y = -32768;
+}
+sf::Packet& operator <<(sf::Packet& paquet, const Position& pos) {
+    paquet << pos.x << pos.y;
+    return paquet;
+}
+sf::Packet& operator >>(sf::Packet& paquet, const Position& pos) {
+    paquet >> pos.x >> pos.y;
+    return paquet;
 }
