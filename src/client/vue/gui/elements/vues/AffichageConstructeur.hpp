9f053b34f510f6b30b2ce6437a249e83b0dbc6db
Kowah <kornarak@gmail.com>
2014-01-23 17:34:49 +0100

    kikoo

---
+++ src/client/vue/gui/elements/vues/AffichageConstructeur.hpp
@@ -0,0 +1,17 @@
+/*
+ * AffichageConstructeur.hpp
+ *
+ *  Created on: 23 janv. 2014
+ *      Author: Gabriel
+ */
+
+#ifndef AFFICHAGECONSTRUCTEUR_HPP_
+#define AFFICHAGECONSTRUCTEUR_HPP_
+
+class AffichageConstructeur {
+public:
+	AffichageConstructeur();
+	virtual ~AffichageConstructeur();
+};
+
+#endif /* AFFICHAGECONSTRUCTEUR_HPP_ */
