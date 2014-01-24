e7fd1aa8b821c0e3042f94e0426bc2beb4b5a385
Soinou <soinouspirit@gmail.com>
2014-01-24 11:12:54 +0100

    Fix le temps que ça remarche

--- src/commun/ReseauClient.cpp
+++ src/commun/ReseauClient.cpp
@@ -1,10 +1,7 @@
 #include "ReseauClient.hpp"
 
-ReseauClient::ReseauClient(Plateau& _plateau, Joueur& _joueur) :
-    ip("none"),
-    port(0),
-    plateau(_plateau),
-    joueur(_joueur) {
+ReseauClient::ReseauClient(Plateau& _plateau, Joueur& _joueur)
+                : ip("none"), port(0), plateau(_plateau), joueur(_joueur) {
     setActif(false);
 }
 
@@ -17,17 +14,13 @@ void ReseauClient::ConnexionServeur(string ip, unsigned short port) {
     sf::Time timeout = sf::seconds(5);
 
     if (getActif() == true) {
-        cout
-                << "[RESEAU] Vous êtes déjà connecté à un serveur !"
-                << endl;
+        cout << "[RESEAU] Vous êtes déjà connecté à un serveur !" << endl;
         return;
     }
 
     // On se connecte au serveur
     if (socket.connect(server, port, timeout) != sf::Socket::Done) {
-        cout
-                << "[RESEAU] Impossible de se connecter au serveur !"
-                << endl;
+        cout << "[RESEAU] Impossible de se connecter au serveur !" << endl;
         return;
     }
 
@@ -37,10 +30,7 @@ void ReseauClient::ConnexionServeur(string ip, unsigned short port) {
     // On définit que le réseau est maintenant actif
     setActif(true);
 
-    cout
-            << "[RESEAU] Connecté au serveur "
-            << server
-            << endl;
+    cout << "[RESEAU] Connecté au serveur " << server << endl;
 }
 
 void ReseauClient::TraiterPaquetServeur(void) {
@@ -62,61 +52,54 @@ void ReseauClient::TraiterPaquetServeur(void) {
 
     switch (static_cast<TypePaquet>(typePaquet)) {
 
-    case TypePaquet::Plateau:
-        paquet >> plateau;
-        break;
+        case TypePaquet::Plateau:
+            paquet >> plateau;
+            break;
 
-    case TypePaquet::MessageEchoServeur:
-        paquet >> message;
-        cout
-                << "[SERVEUR] "
-                << message
-                << endl;
-        break;
+        case TypePaquet::MessageEchoServeur:
+            paquet >> message;
+            cout << "[SERVEUR] " << message << endl;
+            break;
 
-    case TypePaquet::ZoneParcourable:
-        parseZoneParcourable(paquet);
-        break;
+        case TypePaquet::ZoneParcourable:
+            parseZoneParcourable(paquet);
+            break;
 
-    case TypePaquet::Chemin:
-        parseChemin(paquet);
-        break;
+        case TypePaquet::Chemin:
+            parseChemin(paquet);
+            break;
 
-    case TypePaquet::DeplacerVaisseau:
-        deplacerVaisseau(paquet);
-        break;
+        case TypePaquet::DeplacerVaisseau:
+            deplacerVaisseau(paquet);
+            break;
 
-    case TypePaquet::DeplacementVaisseauImpossible:
-        break;
+        case TypePaquet::DeplacementVaisseauImpossible:
+            break;
 
-    case TypePaquet::ZoneConstructibleVaisseau:
-        parseZoneConstructibleVaisseau(paquet);
-        break;
+        case TypePaquet::ZoneConstructibleVaisseau:
+            parseZoneConstructibleVaisseau(paquet);
+            break;
 
-    case TypePaquet::ZoneConstructibleBatiment:
-        parseZoneConstructibleBatiment(paquet);
-        break;
+        case TypePaquet::ZoneConstructibleBatiment:
+            parseZoneConstructibleBatiment(paquet);
+            break;
 
-	case TypePaquet::ZoneAttaquable:
-		parseZoneAttaquable(paquet);
-		break;
+        case TypePaquet::ZoneAttaquable:
+            parseZoneAttaquable(paquet);
+            break;
 
-	case TypePaquet::JoueurCourant:
-		parseJoueurCourant(paquet);
-		break;
+        case TypePaquet::JoueurCourant:
+            parseJoueurCourant(paquet);
+            break;
 
-	case TypePaquet::JoueursAdverses:
-		parseJoueursAdverses(paquet);
-		break;
+        case TypePaquet::JoueursAdverses:
+            parseJoueursAdverses(paquet);
+            break;
 
-	default:
-		cout
-		        << "[RESEAU] Erreur: paquet de type "
-		        << typePaquet
-		        << " inconnu"
-		        << endl;
-		break;
-	}
+        default:
+            cout << "[RESEAU] Erreur: paquet de type " << typePaquet << " inconnu" << endl;
+            break;
+    }
 }
 
 /// Après la connexion, on dit bonjour au serveur
@@ -125,9 +108,7 @@ void ReseauClient::EnvoyerPseudoServeur(string pseudo) {
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::ChangementDeNom);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet
-            << pseudo;
+    paquet << typePaquet << pseudo;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
@@ -136,8 +117,7 @@ void ReseauClient::getZoneParcourable(Position p) {
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetZoneParcourable);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet << p;
+    paquet << typePaquet << p;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
@@ -167,7 +147,6 @@ void ReseauClient::parseChemin(sf::Packet paquet) {
     }
 }
 
-
 void ReseauClient::parseZoneConstructibleVaisseau(sf::Packet paquet) {
     sf::Int32 tailleZone;
     std::list<Position> pos;
@@ -194,41 +173,39 @@ void ReseauClient::parseZoneConstructibleBatiment(sf::Packet paquet) {
     }
 }
 
-void ReseauClient::parseZoneAttaquable(sf::Packet paquet){
-	sf::Int32 tailleZone;
-	std::list<Position> pos;
-	Position p;
+void ReseauClient::parseZoneAttaquable(sf::Packet paquet) {
+    sf::Int32 tailleZone;
+    std::list<Position> pos;
+    Position p;
 
-	paquet >> tailleZone;
+    paquet >> tailleZone;
 
-	for (sf::Int32 i = 0; i < tailleZone; i++)
-	{
-		paquet >> p;
-		plateau.cellule[p.x][p.y].setEstAttaquable(true);
-	}
+    for (sf::Int32 i = 0; i < tailleZone; i++) {
+        paquet >> p;
+        plateau.cellule[p.x][p.y].setEstAttaquable(true);
+    }
 }
 
 void ReseauClient::parseJoueurCourant(sf::Packet paquet) {
-	paquet >> joueur;
+    paquet >> joueur;
 }
 
 void ReseauClient::parseJoueursAdverses(sf::Packet paquet) {
-	sf::Int32 tailleListe;
-	string j;
-	vector<string> joueursAdverses;
+    sf::Int32 tailleListe;
+    string j;
+    vector<string> joueursAdverses;
 
-	paquet >> tailleListe;
+    paquet >> tailleListe;
 
-	for (sf::Int32 i = 0; i < tailleListe; i++)
-	{
-		paquet >> j;
-		joueursAdverses.push_back(j);
-	}
+    for (sf::Int32 i = 0; i < tailleListe; i++) {
+        paquet >> j;
+        joueursAdverses.push_back(j);
+    }
 
-	// TODO
+    // TODO
 }
 
-void ReseauClient::deplacerVaisseau(sf::Packet){
+void ReseauClient::deplacerVaisseau(sf::Packet) {
 
 }
 
@@ -236,8 +213,7 @@ void ReseauClient::getChemin(Position depart, Position arrivee) {
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetChemin);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet << depart << arrivee;
+    paquet << typePaquet << depart << arrivee;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
@@ -246,8 +222,7 @@ void ReseauClient::demanderDeplacementVaisseau(Position depart, Position arrivee
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::DemanderDeplacementVaisseau);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet << depart << arrivee;
+    paquet << typePaquet << depart << arrivee;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
@@ -256,8 +231,7 @@ void ReseauClient::getZoneConstructibleVaisseau(Position p) {
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetZoneConstructibleVaisseau);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet << p;
+    paquet << typePaquet << p;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
@@ -266,40 +240,36 @@ void ReseauClient::getZoneConstructibleBatiment(Position p) {
     sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetZoneConstructibleBatiment);
     sf::Packet paquet;
 
-    paquet
-            << typePaquet << p;
+    paquet << typePaquet << p;
 
     ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
 
 void ReseauClient::getZoneAttaquable(Position p) {
-	sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetZoneAttaquable);
-	sf::Packet paquet;
+    sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetZoneAttaquable);
+    sf::Packet paquet;
 
-	paquet
-	        << typePaquet << p;
+    paquet << typePaquet << p;
 
-	ReseauGlobal::EnvoiPaquet(socket, paquet);
+    ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
 
 void ReseauClient::getJoueurCourant() {
-	sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetJoueurCourant);
-	sf::Packet paquet;
+    sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetJoueurCourant);
+    sf::Packet paquet;
 
-	paquet
-	        << typePaquet;
+    paquet << typePaquet;
 
-	ReseauGlobal::EnvoiPaquet(socket, paquet);
+    ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
 
 void ReseauClient::getJoueursAdverses() {
-	sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetJoueursAdverses);
-	sf::Packet paquet;
+    sf::Uint16 typePaquet = static_cast<sf::Uint16>(TypePaquet::GetJoueursAdverses);
+    sf::Packet paquet;
 
-	paquet
-	        << typePaquet;
+    paquet << typePaquet;
 
-	ReseauGlobal::EnvoiPaquet(socket, paquet);
+    ReseauGlobal::EnvoiPaquet(socket, paquet);
 }
 
 void ReseauClient::setActif(bool _actif) {
--- src/commun/ReseauClient.hpp
+++ src/commun/ReseauClient.hpp
@@ -14,101 +14,101 @@
  */
 class ReseauClient {
 
-public:
-	/**
-	 * \brief Constructeur par défaut
-	 *
-	 */
-	ReseauClient(Plateau& _plateau, Joueur& _joueur);
+    public:
+        /**
+         * \brief Constructeur par défaut
+         *
+         */
+        ReseauClient(Plateau& _plateau, Joueur& _joueur);
 
-	/**
-	 * \brief Destructeur par défaut
-	 */
-	~ReseauClient(void);
+        /**
+         * \brief Destructeur par défaut
+         */
+        ~ReseauClient(void);
 
-	/**
-	 * \brief Connexion à un serveur
-	 *
-	 * \param ip l'ip du serveur sur lequel on veut se connecter
-	 * \param port le port du serveur
-	 */
-	void ConnexionServeur(string ip, unsigned short port);
+        /**
+         * \brief Connexion à un serveur
+         *
+         * \param ip l'ip du serveur sur lequel on veut se connecter
+         * \param port le port du serveur
+         */
+        void ConnexionServeur(string ip, unsigned short port);
 
-	/**
-	 * \brief Traite un paquet reçu de la part du serveur
-	 *
-	 */
-	void TraiterPaquetServeur(void);
+        /**
+         * \brief Traite un paquet reçu de la part du serveur
+         *
+         */
+        void TraiterPaquetServeur(void);
 
-	/**
-	 * \brief Récupère l'état d'activité du client sur le réseau
-	 *
-	 * \return si le client est actif ou non
-	 */
-	bool getActif(void);
+        /**
+         * \brief Récupère l'état d'activité du client sur le réseau
+         *
+         * \return si le client est actif ou non
+         */
+        bool getActif(void);
 
-	/**
-	 * \brief Définit l'état d'activité du client sur le réseau
-	 *
-	 * \param _actif l'état d'activité
-	 */
-	void setActif(bool _actif);
+        /**
+         * \brief Définit l'état d'activité du client sur le réseau
+         *
+         * \param _actif l'état d'activité
+         */
+        void setActif(bool _actif);
 
-	sf::TcpSocket& getSocket(void);
+        sf::TcpSocket& getSocket(void);
 
-	void setIp(string _ip);
+        void setIp(string _ip);
 
-	void setPort(unsigned short _port);
+        void setPort(unsigned short _port);
 
-	void EnvoyerPseudoServeur(string pseudo);
+        void EnvoyerPseudoServeur(string pseudo);
 
-	void getZoneParcourable(Position p);
+        void getZoneParcourable(Position p);
 
-	void parseZoneParcourable(sf::Packet paquet);
+        void parseZoneParcourable(sf::Packet paquet);
 
-	void getChemin(Position depart, Position arrivee);
+        void getChemin(Position depart, Position arrivee);
 
-	void parseChemin(sf::Packet paquet);
+        void parseChemin(sf::Packet paquet);
 
-	void deplacerVaisseau(sf::Packet paquet);
+        void deplacerVaisseau(sf::Packet paquet);
 
-	void demanderDeplacementVaisseau(Position depart, Position arrivee);
+        void demanderDeplacementVaisseau(Position depart, Position arrivee);
 
-	void getZoneConstructibleVaisseau(Position p);
+        void getZoneConstructibleVaisseau(Position p);
 
-	void getZoneConstructibleBatiment(Position p);
+        void getZoneConstructibleBatiment(Position p);
 
-	void parseZoneConstructibleVaisseau(sf::Packet paquet);
+        void parseZoneConstructibleVaisseau(sf::Packet paquet);
 
-	void parseZoneConstructibleBatiment(sf::Packet paquet);
+        void parseZoneConstructibleBatiment(sf::Packet paquet);
 
-	void getZoneAttaquable(Position p);
+        void getZoneAttaquable(Position p);
 
-	void parseZoneAttaquable(sf::Packet paquet);
+        void parseZoneAttaquable(sf::Packet paquet);
 
-	void getJoueurCourant();
+        void getJoueurCourant();
 
-	void getJoueursAdverses();
+        void getJoueursAdverses();
 
-	void parseJoueurCourant(sf::Packet paquet);
-	
-	void parseJoueursAdverses(sf::Packet paquet);
+        void parseJoueurCourant(sf::Packet paquet);
 
-private:
+        void parseJoueursAdverses(sf::Packet paquet);
 
-	/// La socket du client
-	sf::TcpSocket socket;
+    private:
 
-	/// Permet de savoir si le réseau est actif (ie. le client est connecté à un serveur)
-	bool actif;
+        /// La socket du client
+        sf::TcpSocket socket;
 
-	string ip;
+        /// Permet de savoir si le réseau est actif (ie. le client est connecté à un serveur)
+        bool actif;
 
-	unsigned short port;
+        string ip;
 
-	Plateau& plateau;
+        unsigned short port;
 
-	Joueur& joueur;
+        Plateau& plateau;
+
+        Joueur& joueur;
 };
 
 #endif
