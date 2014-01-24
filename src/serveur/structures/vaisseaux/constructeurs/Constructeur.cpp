f2b4c4679d27f1ec62053c3ec6347c50a4007365
FredericCosnefroy <frederic.cosnefroy@gmail.com>
2014-01-24 19:18:56 +0100

    Ajout de valeurs par défaut dans le constructeur du constructeur

--- src/serveur/structures/vaisseaux/constructeurs/Constructeur.cpp
+++ src/serveur/structures/vaisseaux/constructeurs/Constructeur.cpp
@@ -1,12 +1,6 @@
 #include "Constructeur.hpp"
 
 
-Constructeur::Constructeur() : VaisseauServeur(){
-
-    ajouterBatiment(BatimentServeur(1500, 300, 0.4f, 120, 0, 1, 0, 1000, 1000, TypeBatiment::Bidon));
-    ajouterBatiment(BatimentServeur(1100, 100, 0.4f, 250, 350, 1, 100, 1500, 1000, TypeBatiment::Bidon));
-    ajouterBatiment(BatimentServeur(900, 700, 0.8f, 100, 0, 1, 0, 700, 1000, TypeBatiment::Bidon));
-}
 
 Constructeur::Constructeur(sf::Int32 vieMax, sf::Int32 bouclierMax, float bouclierTaux, sf::Int32 visibilite, sf::Int32 attaque, sf::Int32 energieMax, sf::Int32 consommationMax, sf::Int32 rayonAttaque, sf::Int32 prixEnergie, sf::Int32 prixMateriaux) :
     VaisseauServeur(vieMax, bouclierMax, bouclierTaux, visibilite, attaque, energieMax, consommationMax, rayonAttaque, prixEnergie, prixMateriaux, TypeVaisseau::Constructeur) {
--- src/serveur/structures/vaisseaux/constructeurs/Constructeur.hpp
+++ src/serveur/structures/vaisseaux/constructeurs/Constructeur.hpp
@@ -14,8 +14,7 @@
 class Constructeur : public VaisseauServeur {
 
     public:
-    Constructeur();
-    Constructeur(sf::Int32 vieMax, sf::Int32 bouclierMax, float bouclierTaux, sf::Int32 visibilite, sf::Int32 attaque, sf::Int32 energieMax, sf::Int32 consommationMax, sf::Int32 rayonAttaque, sf::Int32 prixEnergie, sf::Int32 prixMateriaux);
+    Constructeur(sf::Int32 vieMax = 100, sf::Int32 bouclierMax = 100, float bouclierTaux = 0.3f, sf::Int32 visibilite = 100, sf::Int32 attaque = 100, sf::Int32 energieMax = 200, sf::Int32 consommationMax = 100, sf::Int32 rayonAttaque = 100, sf::Int32 prixEnergie = 500, sf::Int32 prixMateriaux = 1000);
     BatimentServeur getBatiment(int index);
     void ajouterBatiment(BatimentServeur batiment);
     void retirerBatiment();
