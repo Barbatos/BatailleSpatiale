3bbad4e4480d61f83b6add7bbb976a6b75dbf050
Charles Duprey <barbatos@urbanterror.info>
2014-01-28 23:05:22 +0100

    Fonction main() du Master Serveur

---
+++ src/masterserveur/mainMaster.cpp
@@ -0,0 +1,33 @@
+#include "Master.hpp"
+
+int main() {
+    unsigned int portServeurs = 1500;
+    unsigned int portClients = 1600;
+    Master* masterServeur;
+    string msg = "";
+
+    cout << "Vous êtes sur le point de lancer un Master Server pour le jeu Bataille Spatiale." << endl;
+    cout << "Veuillez entrer le port sur lequel écouter les serveurs de jeu [" << portServeurs << "]" << endl;
+
+    getline(cin, msg);
+
+    if(!msg.empty()) {
+        portServeurs = atoi(msg.c_str());
+    }
+
+    cout << "Veuillez entrer le port sur lequel écouter les clients de jeu [" << portClients << "]" << endl;
+
+    getline(cin, msg);
+
+    if(!msg.empty()) {
+        portClients = atoi(msg.c_str());
+    }
+
+    masterServeur = new Master(portServeurs, portClients);
+
+    while(true) {
+    	masterServeur->ecouterReseau();
+    	masterServeur->verifierServeursActifs();
+    	sf::sleep(sf::milliseconds(50));
+    }
+}
