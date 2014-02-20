13be37fb8a924f68c1f4a0d45ae6c6911427c7d5
Charles Duprey <barbatos@urbanterror.info>
2014-02-20 18:17:00 +0100

    Augmentation du timeout de la connexion au Master

--- src/commun/ReseauClient.cpp
+++ src/commun/ReseauClient.cpp
@@ -65,7 +65,7 @@ void ReseauClient::ConnexionServeur(string ip, unsigned short port,
 void ReseauClient::ConnexionMasterServeur(void) {
     unsigned short portMaster = 1600;
     sf::IpAddress masterServer("barbatos.fr");
-    sf::Time timeout = sf::seconds(0.05);
+    sf::Time timeout = sf::seconds(0.15);
     int nbEssais = 0;
 
     while (socketMaster.connect(masterServer, portMaster, timeout)
